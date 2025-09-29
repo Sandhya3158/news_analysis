dea7f903a8ab5c8ce0c1efa2b3b0dda333c61364
Sandhya3158 <sandhyamadhunagula08@gmail.com>
2025-09-30 00:13:28 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -108,4 +108,5 @@ def download_csv():
     return send_file('grouped_data.csv', as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5050, debug=True)
+    #app.run(host='127.0.0.1', port=5050, debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
